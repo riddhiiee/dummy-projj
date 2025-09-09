0c7ac6fe438015a63ad73e5d942061c78ff06224
Riddhi Bhagat <140200617+riddhiiee@users.noreply.github.com>
2025-09-09 11:50:29 +0530

    steps added

--- app.py
+++ app.py
@@ -16,3 +16,14 @@ x2 = st.number_input("Enter X2 (0 or 1)", min_value=0, max_value=1, step=1)
 if st.button("Predict"):
     pred = model.predict(np.array([[x1, x2]]))[0][0]
     st.write(f"Prediction: {round(pred)} (Probability: {pred:.4f})")
+
+
+# STEPS:
+# 1. git init
+# 2. make train.py and app.py
+# 3. git add .
+# 4. git commiy -m "some message"
+# 5. git branch -M main
+# 6. git remote add origin https://github.com/riddhiiee/dummy-projj.git 
+# 7. git push -u origin main
+# 8. git push -u origin main --force
